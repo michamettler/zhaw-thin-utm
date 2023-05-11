a9eb7f22f2eb9745dc5eeca9b19049381640645a
michamettler <mettlermicha@gmail.com>
2023-05-11 21:26:13 +0200

    finished state parsing based on encoded turing machine

--- ZHAW-THIN-UTM/src/main/java/org/example/Main.java
+++ ZHAW-THIN-UTM/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        TuringMachine tm = new TuringMachine("0100100010100");
+        TuringMachine tm = new TuringMachine("010010001010011000101010010110001001001010011000100010001010");
         tm.run();
     }
 }
--- ZHAW-THIN-UTM/src/main/java/org/example/TuringMachine.java
+++ ZHAW-THIN-UTM/src/main/java/org/example/TuringMachine.java
@@ -23,16 +23,14 @@ public class TuringMachine {
     }
 
     public void run() {
-        while(code.hasNext()) {
-            String nextSymbol;
-            do {
-                nextSymbol = code.next();
-                CompositionFrom from = new CompositionFrom(new State(readState(), false), readSymbol());
-                CompositionTo to = new CompositionTo(new State(readState(), false), readSymbol(), readDirection());
-                Transition transition = new Transition(from, to);
-                System.out.println(transition);
-            } while (code.hasNext() && nextSymbol.equals("11"));
-        }
+        do {
+            CompositionFrom from = new CompositionFrom(new State(readState(), false), readSymbol());
+            CompositionTo to = new CompositionTo(new State(readState(), false), readSymbol(), readDirection());
+            Transition transition = new Transition(from, to);
+            System.out.println(transition);
+
+            checkTransition();
+        } while (code.hasNext());
     }
 
     private int readState() {
@@ -46,22 +44,35 @@ public class TuringMachine {
     private Alphabet readSymbol() {
         StringBuilder symbols = new StringBuilder();
         String nextSymbol = code.next();
+
         while (code.hasNext() && !nextSymbol.equals("1")) {
             symbols.append(nextSymbol);
             nextSymbol = code.next();
         }
+
         return Alphabet.valueOf(Objects.requireNonNull(SymbolMapping.findEnum(symbols.toString())).name());
     }
 
     private Direction readDirection() {
         StringBuilder symbols = new StringBuilder();
         String nextSymbol = code.next();
+
         while (code.hasNext() && !nextSymbol.equals("1")) {
             symbols.append(nextSymbol);
             nextSymbol = code.next();
         }
+
+        if (!code.hasNext() && !nextSymbol.equals("1")) {
+            symbols.append(nextSymbol);
+        }
+
         return Direction.findEnum(symbols.toString());
     }
 
+    private void checkTransition() {
+        if (code.hasNext() && !code.next().equals("1")) {
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
